63ccf5418665c355a80d069ecf0523c4d83351b2
ismaelp96 <ismael6991@live.com>
2024-10-23 16:24:48 -0300

    refator: card agora é feito atráves de diretivas tanto no header como content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { CardContentDirective } from './card/directives/card-content.directive';
+import { CardHeaderDirective } from './card/directives/card-header.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    CardContentDirective,
+    CardHeaderDirective
   ],
   imports: [
     BrowserModule,
